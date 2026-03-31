498da6081eb22769892e8d0a7a6f28b0466dde04
Vansh Garg <vansgarg55@gmail.com>
2026-03-31 13:09:57 +0530

    feat: Repository section

--- src/modules/auth/github/lib/github.ts
+++ src/modules/auth/github/lib/github.ts
@@ -137,3 +137,21 @@ export async function fetchUserContribution(token: string, username: string) {
 		);
 	}
 }
+
+export const getRepositories = async (
+	page: number = 1,
+	perPage: number = 10
+) => {
+	const token = await getGithubAccessToken();
+	const octokit = new Octokit({ auth: token });
+
+	const { data } = await octokit.rest.repos.listForAuthenticatedUser({
+		sort: "updated",
+		direction: "desc",
+		visibility: "all",
+		per_page: perPage,
+		page: page,
+	});
+
+	return data;
+};
